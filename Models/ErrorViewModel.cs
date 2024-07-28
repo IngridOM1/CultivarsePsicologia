b8588c69773198308670a7c4bf8bc13f99a2ad42
Ingrid Ochoa <ingrid_ochoa@usmp.pe>
2024-07-28 16:48:37 -0500

    se instalo el ofrmato .NET

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace cultivarsepsicologia.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
